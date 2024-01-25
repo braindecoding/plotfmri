f23ff358a18bf6e27949c85560a178b83e26d3a1
Rolly Maulana  Awangga <rolly@awang.ga>
2024-01-25 15:11:01 +0700

    roi

--- helper/plot.py
+++ helper/plot.py
@@ -21,3 +21,16 @@ def plotAll(folder_path,target_extension,target_path,new_extension):
         new_filename = os.path.splitext(file_name)[0] + new_extension
         plt.savefig(target_path+new_filename)
         plotting.show()
+        
+def plotAllMask(folder_path,target_extension,target_path,new_extension):
+    file_list = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f)) and f.endswith(target_extension)]
+    
+    # Load fMRI data in Analyze format
+    
+    for file_name in file_list:
+        img = nib.load(folder_path+file_name)
+        # Plot the functional MRI data
+        plotting.plot_roi(img,title=file_name)
+        new_filename = os.path.splitext(file_name)[0] + new_extension
+        plt.savefig(target_path+new_filename)
+        plotting.show()
--- main.py
+++ main.py
@@ -1,54 +1,74 @@
 import matplotlib.pyplot as plt
 import nibabel as nib
 from nilearn import plotting
+import numpy as np
+from helper.plot import plotAllMask
 
-# In[]: Config
+
+# In[]: Config roi data
+target_path="roim/"
+folder_path="roi/"
+target_extension = '.img'
+new_extension = '.png'
+sbjId = 's1'
+#plot all
+plotAllMask(folder_path,target_extension,target_path,new_extension)
+
+
+# In[]: Config fmri data
 target_path="img/"
 folder_path="data/"
 target_extension = '.img'
 new_extension = '.png'
 sbjId = 's1'
-
+#plot all
+#plotAll(folder_path,target_extension,target_path,new_extension)
 
 # In[]: Load fMRI data in Analyze format
 fmri_image  = nib.load(folder_path+'ras1_c0005'+target_extension)
 fmri_data = fmri_image.get_fdata()
+
+# In[]: atlas mask https://nilearn.github.io/dev/auto_examples/01_plotting/plot_atlas.html
+from nilearn import datasets
+
+dataset_ho = datasets.fetch_atlas_harvard_oxford("cort-maxprob-thr25-2mm")
+atlas_ho_filename = dataset_ho.filename
+print(f"Atlas ROIs are located at: {atlas_ho_filename}")
+# In[]: 
+plotting.plot_roi(atlas_ho_filename, title="Harvard Oxford atlas")
+plotting.plot_roi(
+    atlas_ho_filename,
+    view_type="contours",
+    title="Harvard Oxford atlas in contours",
+)
+plotting.show()
+
 # In[]: Plot all brain
-plotting.plot_epi(fmri_image)
+plotting.plot_roi(fmri_image)
+#plotting.plot_stat_map(fmri_image)
 plotting.show()
 
 # In[]:Plot on glass brain with visual cortex activation
 plotting.plot_glass_brain(fmri_image, threshold=3.0, colorbar=True, plot_abs=False, display_mode='lyrz')
 
 # You can also add anatomical or functional atlases to highlight visual cortices
-plotting.add_atlas_to_plot(atlas_name='Harvard-Oxford Visual Cortices', threshold=1, colorbar=True)
+#plotting.add_atlas_to_plot(atlas_name='Harvard-Oxford Visual Cortices', threshold=1, colorbar=True)
 
 # Show the plot
 plotting.show()
 
 # In[]: Load into fMRI Visual Cortex, Plot the loaded image on a glass brain view
-from scipy.io import loadmat
-#roiBaseName = sprintf('s1_BAIC3T_%s_ang_dm1to2_%s_', 'LH', 'LH');
-#roiOrder = {'V1v', 'V2v', 'VP', 'V4v', 'V1d', 'V2d', 'V3', 'V3A'};
-#roiBaseName = sprintf('s1_BAIC3T_%s_ang_dm1to2_%s_', 'RH', 'RH');
-#roiOrder = {'V1v', 'V2v', 'VP', 'V4v', 'V1d', 'V2d', 'V3', 'V3A'};
-roifolder="roi/"
-roilname="unq_s1_BAIC3T_LH_ang_dm1to2_%s_"
-# Load the MAT-file containing ROI masks
-mat_data = loadmat('roi/VOX_s1_BAIC3T_LH_ang_dm1to2_LH_V1v.mat')
-mat_data2 = loadmat('roi/VOX_s1_BAIC3T_RH_ang_dm1to2_RH_V1v.mat')
-# In[]:
-# Access the ROI masks from the loaded dictionary
-roi_mask1 = mat_data['VOX_HU071119_BAIC3T_LH_ang_dm1to2_LH_V1v']
-roi_mask2 = mat_data2['VOX_HU071119_BAIC3T_RH_ang_dm1to2_RH_V1v']
-# In[]:
-roi_mask1 = nib.load('roi/roi_mask1.img').get_fdata()
-roi_mask2 = nib.load('roi/roi_mask2.img').get_fdata()
 
-# In[]:Extract data within ROIs
-roi_data1 = fmri_data[:,roi_mask1 > 0]
-roi_data2 = fmri_data[:,roi_mask2 > 0]
+# Load ROI masks
+roi_mask1 = nib.load('roi/unq_s1_BAIC3T_LH_ang_d-1to2mm_fmr.img')
+roi_mask2 = nib.load('roi/unq_s1_BAIC3T_RH_ang_d-1to2mm_fmr.img')
 
+plotting.plot_roi(roi_mask1,title="Miyawaki atlas in contours")
+#plotting.plot_stat_map(fmri_image)
+plotting.show()
+plotting.plot_roi(roi_mask2)
+#plotting.plot_stat_map(fmri_image)
+plotting.show()
 # In[]: Plot data within ROIs
 plt.figure(figsize=(10, 6))
 
@@ -65,7 +85,6 @@ plt.legend()
 plt.tight_layout()
 plt.show()
 
-#plot all
-#plotAll(folder_path,target_extension,target_path,new_extension)
+
 
 
